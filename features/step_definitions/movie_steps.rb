f3807bfd302af50bc543b6e5b2672374569c4918
Andrés Caro <andres.caro.q@gmail.com>
2013-02-28 14:41:28 -0300

    filter_movie_list.feature completed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -25,4 +25,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(%r{,\s*}).each do |rating|
+    if uncheck.nil?
+      step %{I check "ratings[#{rating}]"}
+    else
+      step %{I uncheck "ratings[#{rating}]"}
+    end
+  end
+end
+
+Then /^I should see all of the movies$/ do
+  page.all('table#movies tr').count.should == 11
 end
